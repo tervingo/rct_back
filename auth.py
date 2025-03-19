e6fe9a39d819c21fc0d2a1b0e7cb63a2e5841942
tervingo <keltivas@gmail.com>
2025-03-19 12:02:16 +0100

    hash de fake_user regenerado

--- auth.py
+++ auth.py
@@ -40,7 +40,7 @@ fake_users_db = {
         "username": "admin",
         "full_name": "Administrator",
         "email": "admin@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",  # "adminpassword"
+        "hashed_password": "$2b$12$BW4DBBcUWLDPqJF4qjIx5O2Ge0AVebglYwPF2TaQeV7B4uUZac9NG",  # "adminpassword"
         "disabled": False,
     }
 }
